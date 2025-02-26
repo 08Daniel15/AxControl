3f19a1911ac4b9eefb6659e406e53b5a9944a158
Daniel <danielrief01@gmail.com>
2025-02-26 21:05:34 +0100

    Create Class Interpolator

---
+++ Cl_Interpolator.cpp
@@ -0,0 +1,15 @@
+class Interpolator
+{
+
+    public:
+        double startPos = 0.0;
+        double endPos = 0.0;
+        
+
+    private:
+        double malsehen = 0.0;
+        int counts = 0;
+
+
+};
+
