6018b0d9917a9a52fa9e4e1a112b19a50e0072a3
Daniel <danielrief01@gmail.com>
2025-02-26 20:53:23 +0100

    Create Hello World

---
+++ testInterpolator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    std::cout<<"Hallo World"<< std::endl;
+
+
+    double ts = 0.001;
+    
+    double startPos = 0.0;
+    double endPos = 10.0;
+    double deltaPos = 0.0;
+    deltaPos = endPos - startPos;
+    
+    std::cout << deltaPos << std::endl;
+    
+
+
+
+    return 0;
+
+}
