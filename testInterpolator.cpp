2c0bbc2117d18303d4783d0d179f28f26b1ac053
Daniel <danielrief01@gmail.com>
2025-03-06 21:46:11 +0100

    Implement Console Input

--- testInterpolator.cpp
+++ testInterpolator.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 #include "Interpolator.cpp"
+#include <string>
 
 int main() {
 
     std::cout<<"Hallo World"<< std::endl;
-
-    
-    //double ts = 0.001;
     
     double startPos = 0.0;
     double endPos = 0.0;
     double deltaPos = 0.0;
-    deltaPos = endPos - startPos;
-    
-    std::cout << deltaPos << std::endl;
-    
-    Interpolator Interpolator1(10.0);
+    deltaPos = endPos - startPos + deltaPos;
+    int z = 0;
+    int count = 5;
+    int itr = 0;
+    std::string test = "";
 
+    std::cout << deltaPos << std::endl;
+    Interpolator Interpolator1(10.0);
     std::cout<<Interpolator1.endPos << std::endl;
 
-
+    
+    for (int z = 0; itr < count; itr++)
+    {
+        z = z +1;
+        std::cout << z << std::endl;
+        std::cin>>test;
+        std::cout <<"hallo"<< std::endl;
+    }
+    
 
     return 0;
+
+    
 
 }
